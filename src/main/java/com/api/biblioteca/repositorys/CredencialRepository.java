7a80d704c4121b9b82e521f1e34d6f6faa7a1148
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-06-15 16:40:51 -0600

    ACTUALIZACION DE NOMBRE DE LA ENTIDAD EN EL REPOSITORIO

--- src/main/java/com/api/biblioteca/repositorys/CredencialRepository.java
+++ src/main/java/com/api/biblioteca/repositorys/CredencialRepository.java
@@ -2,9 +2,9 @@ package com.api.biblioteca.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.api.biblioteca.models.Credenciales;
+import com.api.biblioteca.models.Credencial;
 
 @Repository
-public interface CredencialesRepository extends JpaRepository<Credenciales, Long> {
+public interface CredencialRepository extends JpaRepository<Credencial, Long> {
 
 }
