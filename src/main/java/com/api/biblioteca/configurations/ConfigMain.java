b44f4db2d932cd87c8c9731eb71d88d54c9be11d
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-03 15:36:53 -0600

    CONFIGURACION GLOBAL DE LA API

--- src/main/java/com/api/biblioteca/configurations/ConfigMain.java
+++ src/main/java/com/api/biblioteca/configurations/ConfigMain.java
@@ -1,11 +1,18 @@
 package com.api.biblioteca.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
 
 
 @OpenAPIDefinition(
@@ -19,6 +26,26 @@ import io.swagger.v3.oas.annotations.servers.Server;
 )
 
 @Configuration
+@RequiredArgsConstructor
 public class ConfigMain {
 
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuracion) throws Exception{
+        return configuracion.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 }
