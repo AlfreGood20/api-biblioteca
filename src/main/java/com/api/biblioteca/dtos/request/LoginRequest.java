2cf1837fd88f3a726bda5b2a8d0258abb9b2d783
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-08 14:18:07 -0600

    LOGIN REQUEST PARA RECIBIR DATOS

--- src/main/java/com/api/biblioteca/dtos/request/LoginRequest.java
+++ src/main/java/com/api/biblioteca/dtos/request/LoginRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record LoginRequest(
 
     @NotBlank(message = "Es obligatorio")
-    @Email(message = "Correo invalido, no cumple con el formato.")
+    @Email(message = "Invalido, no cumple con el formato.")
     String correo,
 
     @NotBlank(message = "Es obligatorio")
