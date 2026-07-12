97c26d0e27029cd15b522efd18249cec4855f10e
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-12 13:13:15 -0600

    BUSCAR ESTADO POR NOMBRE

--- src/main/java/com/api/biblioteca/repositorys/EstadoReservaRepository.java
+++ src/main/java/com/api/biblioteca/repositorys/EstadoReservaRepository.java
@@ -1,10 +1,14 @@
 package com.api.biblioteca.repositorys;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.api.biblioteca.enums.EstadoReservaNombre;
 import com.api.biblioteca.models.EstadoReserva;
 
 @Repository
 public interface EstadoReservaRepository extends JpaRepository<EstadoReserva, Long> {
 
+    Optional<EstadoReserva> findByNombre(EstadoReservaNombre nombre);
 }
