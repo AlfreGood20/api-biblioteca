68401270120586a583e5114234920b60c376c533
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-22 16:44:16 -0600

    CAMBIO DE NOMBRE DE PROPIEDAD DE LIBROID A LIBRO_ID

--- src/main/java/com/api/biblioteca/dtos/request/ReservaRequest.java
+++ src/main/java/com/api/biblioteca/dtos/request/ReservaRequest.java
@@ -1,5 +1,6 @@
 package com.api.biblioteca.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -7,10 +8,7 @@ public record ReservaRequest
 (
     @NotNull(message = "Es obligatorio")
     @Positive(message = "El valor tiene que ser positivo")
-    Long usuarioId,
-
-    @NotNull(message = "Es obligatorio")
-    @Positive(message = "El valor tiene que ser positivo")
+    @JsonProperty("libro_id")
     Long libroId
 ) {
 }
